ec2f41909b9dbee52c6b6bc33b392554159ec66b
Gireeshee Pendela <72486122+GireesheePendela@users.noreply.github.com>
2022-07-27 18:28:55 +0530

    created CSS framework

---
+++ components/Footer.js
@@ -0,0 +1,18 @@
+const Footer = () => (
+  <footer className="bg-blue-700 bg-opacity-25 text-lg">
+    <div className="container mx-auto px-8 pt-16 pb-16">
+      <p>
+        Created by{" "}
+        <a
+          href="https://github.com/GireesheePendela"
+          target="GireesheePendela"
+          rel="noopener noreferrer"
+          className="text-blue-400 hover:text-blue-300 focus:text-blue-300 focus:outline-none"
+        >
+          Greg Ives
+        </a>
+      </p>
+    </div>
+  </footer>
+);
+export default Footer;
