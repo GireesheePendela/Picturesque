40f28d1e2219344bc51140ea6c23ecd56c1e22f5
Gireeshee Pendela <72486122+GireesheePendela@users.noreply.github.com>
2022-07-28 19:26:22 +0530

    Rename global styles

---
+++ next.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  webpack(config, { defaultloaders, dev, isServer }) {
+    if (!dev && !isServer) {
+      Object.assign(config.resolve.alias, {
+        react: "preact/compat",
+        "react-dom": "preact/compat",
+      });
+    }
+
+    config.module.rules.push({
+      test: /\.worker.js$/,
+      use: ["worker-loader", defaultLoaders.babel],
+    });
+    return config;
+  },
+};
---
+++ pages/_app.js
@@ -0,0 +1,5 @@
+import "../styles/global.css";
+import "focus-visible";
+
+const StegaPhoto = ({ Component, pageProps }) => <Component {...pageProps} />;
+export default StegaPhoto;
