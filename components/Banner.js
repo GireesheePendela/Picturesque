08ed9a588595d4757006d00f12d0d60bd5d6f9a8
Gireeshee Pendela <72486122+GireesheePendela@users.noreply.github.com>
2022-07-29 17:55:25 +0530

    Update page

--- components/Banner.js
+++ components/Banner.js
@@ -1,12 +1,15 @@
-const Banner = () => (
-  <header className="bg-gradient-to-b from blue-500 to-blue-900">
-    <div className="container xl:max-w-screen-xl mx-auto px-8 pt-32 pb-16">
-      <h1 className="text- 3x1"> Steganographio</h1>
-      <p className="text-4x1 md:text-5x1 lg:text-6x1 pt-8 font-semibold">
-        Hide files inside images
-      </p>
-    </div>
-  </header>
+const Banner = ({ stars }) => (
+    <header className="bg-gradient-to-b from blue-500 to-blue-900">
+        <div className="container xl:max-w-screen-xl mx-auto px-8 pt-32 pb-16">
+            <h1 className="text- 3x1"> Steganographyio</h1>
+            <a
+                href="https://github.com/GireesheePendela"
+                target="_blank"
+                rel="noopener noreferrer"
+            ></a>
+        </div>
+        <div>Hide files inside images.</div>
+    </header>
 );
 
 export default Banner;
--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,18 @@
 const Footer = () => (
-  <footer className="bg-blue-700 bg-opacity-25 text-lg">
-    <div className="container xl:max-w-screen-xl mx-auto px-8 pt-16 pb-16">
-      <p>
-        Created by{" "}
-        <a
-          href="https://github.com/GireesheePendela"
-          target="GireesheePendela"
-          rel="noopener noreferrer"
-          className="text-blue-400 hover:text-blue-300 focus-visible:underline focus:outline-none"
-        >
-          Gireeshee Pendela
-        </a>
-      </p>
-    </div>
-  </footer>
+    <footer className="bg-blue-700 bg-opacity-25 text-lg">
+        <div className="container xl:max-w-screen-xl mx-auto flex flex-col sm:flex-row justify-between px-8 pt-16 pb-16">
+            <p className="mb-4 sm:mb-0">
+                Created by{" "}
+                <a
+                    href="https://github.com/GireesheePendela"
+                    target="GireesheePendela"
+                    rel="noopener noreferrer"
+                    className="text-blue-400 hover:text-blue-300 focus-visible:underline focus:outline-none"
+                >
+                    Gireeshee Pendela
+                </a>
+            </p>
+        </div>
+    </footer>
 );
 export default Footer;
