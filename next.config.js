05f9b96ea808b96d70ec4267ce55a8e5240ec3b6
Gireeshee Pendela <72486122+GireesheePendela@users.noreply.github.com>
2022-08-01 08:11:14 +0530

    Update the js files using ES module

--- next.config.js
+++ next.config.js
@@ -1,16 +1,14 @@
-module.exports = {
-    webpack(config, { defaultloaders, dev, isServer }) {
-        if (!dev && !isServer) {
-            Object.assign(config.resolve.alias, {
-                react: "preact/compat",
-                "react-dom": "preact/compat",
-            });
-        }
-
-        config.module.rules.push({
-            test: /\.worker.js$/,
-            use: ["worker-loader", defaultloaders.babel],
+export function webpack(config, { defaultloaders, dev, isServer }) {
+    if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+            react: "preact/compat",
+            "react-dom": "preact/compat",
         });
-        return config;
-    },
-};
+    }
+
+    config.module.rules.push({
+        test: /\.worker.js$/,
+        use: ["worker-loader", defaultloaders.babel],
+    });
+    return config;
+}
--- postcss.config.js
+++ postcss.config.js
@@ -1,19 +1,17 @@
-module.exports = {
-    plugins: [
-        "tailwindcss",
-        "postcss-focus-visible",
-        "postcss-flexbugs-fixes",
-        [
-            "postcss-preset-env",
-            {
-                autoprefixer: {
-                    flexbox: "no-2009",
-                },
-                stage: 3,
-                features: {
-                    "custom-properties": false,
-                },
+export const plugins = [
+    "tailwindcss",
+    "postcss-focus-visible",
+    "postcss-flexbugs-fixes",
+    [
+        "postcss-preset-env",
+        {
+            autoprefixer: {
+                flexbox: "no-2009",
             },
-        ],
+            stage: 3,
+            features: {
+                "custom-properties": false,
+            },
+        },
     ],
-};
+];
